172656f32e6ab9e930c086c3e2365cacdee3b161
Evangeline-Gifty <evangelinegiftyy@gmail.com>
2026-07-01 13:25:48 +0530

    Delete Infrastructure Automation with Terraform directory

--- Infrastructure Automation with Terraform/Terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "terraform1" {
-    ami = "egami1234567890"
-    instance_type = var.instance_type
-
-    tags = {
-        name = "terraform_infra1"
-    }
-}
--- Infrastructure Automation with Terraform/Terraform/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-    description = "AWS region"
-    type = String
-}
-
-variable "instance_type" {
-    type =  String
-    default = "t2.micro"
-}
